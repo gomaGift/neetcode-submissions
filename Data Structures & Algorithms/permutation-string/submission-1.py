96fb61704d8a38457a44482ad93c4a2d1298eed0
goma_k Gift <108896508+gomaGift@users.noreply.github.com>
2026-06-13 15:42:59 +0200

    Add: permutation-string - submission-1

---
+++ Data Structures & Algorithms/permutation-string/submission-1.py
@@ -0,0 +1,30 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq = Counter(s1)
+        need = len(freq)
+        have = 0
+
+        s1_len = len(s1)
+        
+        char_map = {}
+        left = 0
+        for right, char in enumerate(s2):
+            char_map[char] = char_map.get(char, 0) + 1
+
+            if char in freq and char_map[char] == freq[char]:
+                have += 1
+
+            while have == need:
+                if right - left + 1 == s1_len:
+                   return True
+
+                left_char = s2[left]
+                char_map[left_char] -= 1
+                if left_char in freq and char_map[left_char] < freq[left_char]:
+                    have -= 1
+
+                left += 1
+
+
+        return False
+
