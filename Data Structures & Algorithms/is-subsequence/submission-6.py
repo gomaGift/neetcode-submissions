3ab630b0a40628c78497be149d4a929f2fb1889a
goma_k Gift <108896508+gomaGift@users.noreply.github.com>
2026-06-21 17:50:17 +0200

    Add: is-subsequence - submission-6

---
+++ Data Structures & Algorithms/is-subsequence/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+ def isSubsequence(self, s: str, t: str) -> bool:
+    if not s:
+        return True
+
+    left = 0
+    right = 0
+    while right < len(t):
+        while right < len(t) and left < len(s) and t[right] != s[left]:
+            right += 1
+
+        
+        if left == len(s):
+            break
+        left += 1
+        right += 1
+
+    return left == len(s)
+
+
+          
+
+        
