b3eab6cd44f3e70ac167ebddff4fe67879a2818e
goma_k Gift <108896508+gomaGift@users.noreply.github.com>
2026-07-13 23:28:10 +0200

    Add: minimum-interval-including-query - submission-0

---
+++ Data Structures & Algorithms/minimum-interval-including-query/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minInterval(self, intervals: List[List[int]], queries: List[int]) -> List[int]:
+        
+        intervals.sort()
+        res = []
+        for query in queries:
+            min_interval = float('inf')
+
+            for start, end in intervals:
+                if  start <= query and query <= end:
+                    min_interval = min(min_interval, end - start + 1)
+
+            min_interval = min_interval if min_interval != float('inf') else -1
+            res.append(min_interval)
+
+        return res
